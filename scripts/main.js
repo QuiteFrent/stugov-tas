e0431231ba4dd6fc7bb18f55c365e4c1ce8c935f
Frentalie <17377256+Frentalie@users.noreply.github.com>
2019-05-12 23:57:54 +0800

    Add new about sections and change style

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    $("mySidebar").style.width = "250px";
+    $("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    $("mySidebar").style.width = "0";
+    $("main").style.marginLeft = "0";
+}
