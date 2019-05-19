5130fadfb0ccb06201ea35368e349b2cfa17055f
Frentalie <17377256+Frentalie@users.noreply.github.com>
2019-05-19 23:30:32 +0800

    Add contact section

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,8 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    $("mySidebar").style.width = "250px";
-    $("main").style.marginLeft = "250px";
-}
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    $("mySidebar").style.width = "0";
-    $("main").style.marginLeft = "0";
-}
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
+<div class="form-group">
+<label for="inputBody">Message</label>
+<textarea class="form-control" id="inputBody" rows="5"></textarea>
+</div>
+<button type="submit" class="btn btn-primary">Submit</button>
