d3754ab2d28396f33274b1500c0c13b7e3b5cdea
AtlasPixels <17377256+Frentalie@users.noreply.github.com>
2019-05-20 09:54:17 +0800

    Fix popover not showing up and member alignment on mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,3 @@
 $(function () {
     $('[data-toggle="popover"]').popover()
-})
-<div class="form-group">
-<label for="inputBody">Message</label>
-<textarea class="form-control" id="inputBody" rows="5"></textarea>
-</div>
-<button type="submit" class="btn btn-primary">Submit</button>
+});
