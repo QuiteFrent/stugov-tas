1aedc802922e8cd21f31c33b42cd0fb1805b9b5a
AtlasPixels <17377256+Frentalie@users.noreply.github.com>
2019-05-20 10:32:01 +0800

    Adjustment to about btn

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
 $(function () {
     $('[data-toggle="popover"]').popover()
 });
+
+$( ".overlay" ).hover(
+    function() {
+      $(this).parents("#team").css("background-color", "#1ED171");
+    }, function() {
+      $(this).parents("#team").css("background-color", "#5CD895");
+    }
+  );
